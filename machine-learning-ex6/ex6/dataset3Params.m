7ec0437af6f90463d4a2e4747ab3aa1dd96d3105
= <shisihang@nuaa.edu.cn>
2017-09-07 11:10:28 +0800

    add files
    add files

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,70 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+C_list = [0.01;0.03;0.1;0.3;1;3;10;30];
+sigma_list = [0.01;0.03;0.1;0.3;1;3;10;30];
+C_size = size(C_list, 1);
+sigma_size = size(sigma_list, 1);
+pred_error = inf;
+for i=1:C_size
+    for j=1:sigma_size
+        local_C = C_list(i);
+        local_sigma = sigma_list(j);
+        model= svmTrain(X, y, local_C, @(x1, x2) gaussianKernel(x1, x2, local_sigma));
+        predictions = svmPredict(model, Xval);
+        error  = mean(double(predictions ~= yval));
+         if error < pred_error
+            pred_error = error;
+            C = local_C;
+            sigma = local_sigma;
+         end
+    end
+end
+        
+% maxError = Inf;
+% 
+% % It would be nice if this didn't do function optimization by exhaustive search :-/
+% for currC = [0.01 0.03 0.1 0.3 1 3 10 30]
+%   for currSigma = [0.01 0.03 0.1 0.3 1 3 10 30]
+%     model = svmTrain(X, y, currC, @(x1, x2) gaussianKernel(x1, x2, currSigma)); 
+% 
+%     predictions = svmPredict(model, Xval);
+%     predictionError = mean(double(predictions ~= yval));
+% 
+%     if predictionError < maxError
+%       maxError = predictionError;
+%       C = currC;
+%       sigma = currSigma;
+%     end
+%   end
+% end
+
+
+
+
+
+
+% =========================================================================
+
+end
